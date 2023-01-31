c8cdd93c6ed70879cd7317a55e4d0731fbacd58a
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-31 15:39:13 -0700

    changed sky to a better jpeg

--- metaverse/src/App.js
+++ metaverse/src/App.js
@@ -1,7 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import "aframe";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import sky from "./assets/sky.jpg";
+import sky from "./assets/metaversesky.jpeg";
 import mars from "./models/mars2.glb";
 
 function App() {
@@ -18,9 +18,8 @@ function App() {
       <a-assets>
         <img id="sky" src={sky} />
       </a-assets>
-
       <a-sky color="#FFFFFF" material="src: #sky" rotation="0 0 0"></a-sky>
-      <a-entity id="mars" position="0 0 0" scale="25 25 25"></a-entity>
+      <a-entity id="mars" position="0 0 0" scale="500 500 500"></a-entity>
     </a-scene>
   );
 }

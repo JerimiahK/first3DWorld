7b436260f30460026af30344f150855cf8c8ef5f
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-02-01 18:35:32 -0700

    adding R3F to try to do make a world with that instead of AFrame

--- metaverse/src/App.js
+++ metaverse/src/App.js
@@ -1,35 +1,40 @@
+import { Canvas } from "@react-three/fiber";
 import "aframe";
-import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import "aframe-extras.animation-mixer";
+import * as THREE from "three";
 import sky from "./assets/metaversesky.jpeg";
 import sunSky from "./assets/sunny.jpeg";
 import scene from "./models/greenScene.glb";
 import donut from  "./models/the_great_morpheus.glb";
-import Douglas from "./models/Walking.glb";
+import douglas from "./models/Mutant.gltf";
 
-function App() {
-  //make a class for controlling the character with WASD, space bar to jump, shift to run
+
+
+
+export default function App() {
 
   return (
-    <a-scene>
-      <a-assets>
-        <img id="sky" src={sunSky} />
-      </a-assets>
-      <a-sky color="#FFFFFF" material="src: #sky" rotation="0 0 0"></a-sky>
-      <a-entity gltf-model={scene} scale="1 1 1" position="0 -7 0"></a-entity>
-      {/* <a-entity
-        gltf-model={donut}
-        scale="1 1 1"
-        position="0 0.5 0"
-        animation-mixer="clip: Animation"
-      ></a-entity> */}
-      <a-entity
-        gltf-model={Douglas}
-        scale=".08 .08 .08"
-        position="0 -3 0"
-        animation-mixer="clip: Walking"
-      ></a-entity>
-    </a-scene>
+    // <a-scene>
+    //   <a-assets>
+    //     <img id="sky" src={sunSky} />
+    //   </a-assets>
+    //   <a-sky color="#FFFFFF" material="src: #sky" rotation="0 0 0"></a-sky>
+    //   <a-entity gltf-model={scene} scale="1 1 1" position="0 -7 0"></a-entity>
+    //   <a-entity
+    //     gltf-model={donut}
+    //     scale="1 1 1"
+    //     position="0 0.5 0"
+    //     animation-mixer="clip: Animation"
+    //   ></a-entity>
+    //   <a-entity
+    //     gltf-model={douglas}
+    //     scale=".008 .008 .008"
+    //     position="0 0 0"
+    //     // animation-mixer="clip: Walking"
+    //   ></a-entity>
+    // </a-scene>
+    <Canvas>
+
+    </Canvas>
   );
 }
-
-export default App;
